fd483afa7ce22cab890dd9eaea0c9604dff3ce25
mads-b <mads@baardsgaard.com>
2012-02-23 16:03:59 +0100

    All the activity classes - skeleton

--- src/progark/a15/GameActivity.java
+++ src/progark/a15/GameActivity.java
@@ -3,7 +3,7 @@ package progark.a15;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainMenuActivity extends Activity {
+public class GameActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
